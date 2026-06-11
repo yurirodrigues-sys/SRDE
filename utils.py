e4bcfb0ceb2e4a6ff26ea54c3edcd02802a63ec5
Leon Marcelino <leon.marcelino@ufrpe.br>
2026-06-11 10:57:08 -0300

    Atualizar o utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,10 @@
 import re
-import os
-
-def limpar_tela():
-    # 'cls' para Windows, 'clear' para Linux/Android (Pydroid)
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 def validar_email(email):
     return re.match(r"^[a-z]+\.[a-z]+@ufrpe\.br$", email) is not None
 
 def validar_senha(senha):
     if len(senha) < 8 or not senha.isdigit(): return False
-    
     for i in range(len(senha) - 1):
-        if abs(int(senha[i+1]) - int(senha[i])) <= 1: return False
+        if abs(int(senha[i+1]) - int(senha[i])) == 1: return False
     return True
