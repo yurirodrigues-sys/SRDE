a48bc3b5b29dc8816181640402efb92a4d912afb
Leon Marcelino <leon.marcelino@ufrpe.br>
2026-06-19 21:22:38 -0300

    Atualizar o utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import re
 import os
+import hashlib
 
 def limpar_tela():
     # 'cls' para Windows, 'clear' para Linux/Android (Pydroid)
@@ -14,3 +15,7 @@ def validar_senha(senha):
     for i in range(len(senha) - 1):
         if abs(int(senha[i+1]) - int(senha[i])) <= 1: return False
     return True
+def gerar_hash(senha):
+    """Gera um hash SHA-256 para a senha fornecida."""
+    # O encode() transforma a string em bytes, exigência do hashlib
+    return hashlib.sha256(senha.encode('utf-8')).hexdigest()
